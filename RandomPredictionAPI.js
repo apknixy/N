8a38dfc6c2726537d68119e1f5985fbffef14e96
Apknixy <apknixy@gmail.com>
2025-07-21 13:27:19 +0530

    Delete RandomPredictionAPI.js

--- RandomPredictionAPI.js
+++
@@ -1,8 +0,0 @@
-function doGet() {
-  var sheet = SpreadsheetApp.openById('1tHNnvISnXDaMTBvF3KB3x5bs8Kha2LolHAbnv2H37dw').getSheetByName('Sheet1');
-  var data = sheet.getRange(2, 1, sheet.getLastRow() - 1).getValues(); // skip header
-  var randomIndex = Math.floor(Math.random() * data.length);
-  var prediction = data[randomIndex][0];
-  return ContentService.createTextOutput(JSON.stringify({ "prediction": prediction }))
-    .setMimeType(ContentService.MimeType.JSON);
-}
